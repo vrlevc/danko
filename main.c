82811334ed52e46b4fd30d0217f71f0be733a359
vrlevc <levchenko_victor@gmail.com>
2019-11-29 13:14:31 +0200

    Common functions - DONE. Main support -a, -h, -v options.

--- main.c
+++ main.c
@@ -9,7 +9,9 @@
 
 #include "server.h"
 
-// Description of long options for getopt_long.
+/**
+ * Description of long options for getopt_long.
+ */
 
 static const struct option long_options[] = {
     { "address",        1, NULL, 'a' },
@@ -19,11 +21,15 @@ static const struct option long_options[] = {
     { "verbose",        0, NULL, 'v' },
 };
 
-// Description of short options for getopt_long.
+/**
+ * Description of short options for getopt_long.
+ */
 
 static const char* const short_options = "a:hm:p:v";
 
-// Usage summary text. 
+/**
+ * Usage summary text. 
+ */
 
 static const char* const usage_template =
     "Usage: %s [ options ]\n"
@@ -35,9 +41,11 @@ static const char* const usage_template =
     " -p, --port PORT           Bind to specified port.\n"
     " -v, --verbose             Print verbose messages.\n";
 
-// Print usage information and exit. If IS_ERROR is nonzero, write to
-// stderr and use an error exit code. Otherwise, write to stdout and
-// use a non-error termination code. Does not return.
+/**
+ * Print usage information and exit. If IS_ERROR is nonzero, write to
+ * stderr and use an error exit code. Otherwise, write to stdout and
+ * use a non-error termination code. Does not return.                  
+ */
 
 static void print_usage (int is_error)
 {
@@ -47,5 +55,98 @@ static void print_usage (int is_error)
 
 int main (int argc, char* const argv[])
 {
+    struct in_addr local_address;
+    uint16_t port;
+    int next_option;
+
+    /**
+     * Store the program name, which we will use in error messages. 
+     */
+    program_name = argv[0];
+
+    /**
+     * Set defaults for options.  
+     */
+
+    /* Bind the server to all local addresses, and assige an unused port automaticaly. */
+    local_address.s_addr = INADDR_ANY;
+    port = 0;
+    /* Do not print verbose message. */
+    verbose = 0;
+    /* Load mudules from the directory containing this executable. */
+    module_dir = get_self_executable_directory();
+    assert (module_dir != NULL);
+
+    /**
+     * Parse options.
+     */
+    do {
+        next_option = getopt_long (argc, argv, short_options, long_options, NULL);
+        switch (next_option)
+        {
+        case 'a':
+            /* User specified -a or --address. */
+            {
+                struct hostent* local_host_name;
+
+                /* Look up the hostname the user specified. */
+                local_host_name = gethostbyname (optarg);
+                if (local_host_name == NULL || local_host_name->h_length == 0)
+                    /* Could not resolve the name. */
+                    error (optarg, "invalid host name");
+                else 
+                    /* Host name is OK, so use it. */
+                    local_address.s_addr = *((int*) (local_host_name->h_addr_list[0]));
+            }
+            break;
+        
+        case 'h':
+            /* User specified -h or --help. */
+            print_usage (0);
+
+        case 'm':
+            /* TODO : not impl yet */
+            break;
+
+        case 'p':
+            /* TODO : not impl yet */
+            break;
+        
+        case 'v':
+            /* User specified -v or --verbose. */
+            verbose = 1;
+            break;
+
+        case '?':
+            /* User specified an unrecognized option. */
+            print_usage (1);
+
+        case -1:
+            /* Done with options. */
+            break;
+
+        default:
+            abort();
+        }
+    } while (next_option != -1);
+
+    /**
+     * This program takes no additional arguments. 
+     * Issued an error if the user specified any. 
+     */
+    if (optind != argc)
+        print_usage (1);
+
+    /**
+     * Print the module directory, if we're running verbose.
+     */
+    if (verbose)
+        printf ("modules will be loaded from %s\n", module_dir);
+
+    /**
+     * Run the server.
+     */
+    server_run (local_address, port);
+
     return 0;
 }
---
+++ server.c
@@ -0,0 +1,8 @@
+#include <arpa/inet.h>
+
+#include "server.h"
+
+void server_run (struct in_addr local_address, uint16_t port)
+{
+
+}
